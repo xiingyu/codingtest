772745347ccfcc7fda5b6018b3c2c119859c9bca
SKH_gasan <tls3162@naver.com>
2025-06-10 16:22:31 +0900

    save from gasan

--- silver/silver 2/1912.cpp
+++ silver/silver 2/1912.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <vector>
 
+
 using namespace std;
 
 
 int main(void) {
-    int n,i;
+    int n,i,temp;
     cin >> n;
+    vector<int> arr;
+    vector<int> dp(n);
 
-    
+    for(i = 0; i < n; ++i) {
+        cin >> temp;
+        arr.push_back(temp);
+        cout << arr[i] << endl;
+    }
 
 }
