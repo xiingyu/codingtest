4c2194fecd447e3312892da34c71e5c05f55051c
xiingyu <tls3162@naver.com>
2025-06-11 08:14:17 +0900

    upload from korail

--- silver/silver 3/2579.cpp
+++ silver/silver 3/2579.cpp
@@ -2,19 +2,31 @@
 #include <vector>
 using namespace std;
 
-int compare(int a, int b) {
+int compare(int a, int b ) {
     return (a > b) ? a : b;
 }
 
+
 void dp_making(vector<int>& dp, vector<int>& stair) {
+    int n = dp.size();
+
+    if (n == 1) {
+        dp[0] = stair[0];
+        return;
+    }
+    if (n == 2) {
+        dp[0] = stair[0];
+        dp[1] = stair[0]+ stair[1];
+        return;
+    }
     dp[0] = stair[0];
     dp[1] = stair[0] + stair[1];
-    dp[2] = stair[0] + stair[2];
+    dp[2] = compare(stair[0] + stair[2], stair[1] + stair[2]);
 
 
     for (int i = 3; i < stair.size(); ++i) {
-        dp[i] = compare(dp[i-1], dp[i-2]) + stair[i];
-        cout << dp[i] << endl;
+        dp[i] = compare(dp[i-2], dp[i-3] + stair[i-1]) + stair[i];
+        // cout << i << " " <<dp[i] << endl;
     }
 }
 
