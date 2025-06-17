97ad848ab19ac923e08ac31c04fd993a8ba11f44
SKH_gasan <tls3162@naver.com>
2025-06-17 17:08:14 +0900

    1078 저글링오염 added

---
+++ jungol/silver/1681.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int min;
+int visited[15][15] = {0};
+int map[15][15] = {0};
+
+int main(void) {
+    int i,j,n;
+    int temp;
+
+    scanf("%d", &n);
+    
+    for(i = 0; i < n; ++i) {
+        for(j = 0; j < n; ++j) {
+            scanf("%d",&temp);
+        }
+
+    }
+}
--- jungol/silver/1695.c
+++ jungol/silver/1695.c
@@ -52,8 +52,8 @@ void findDanji(int n) {
 void scanMap(int n) {
     int y,x;
 
-    for (y = 0; y < n; ++y) {
-        for(x = 0; x < n; ++x) {
+    for (y = 1; y <= n; ++y) {
+        for(x = 1; x <= n; ++x) {
             if (visited[y][x] == 1) continue;
             
             visited[y][x] = 1;
@@ -549,4 +549,123 @@ void danji_01(void) {
 
 
 
+#endif
+
+
+//개쩌는 코드의 강사님버전
+#if 0
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int** graph;
+int* ans = NULL;
+int n;
+
+int dfs(int y, int x);
+void memRelease();
+int compare(const void* a, const void* b);
+
+int main() {
+
+    scanf("%d ", &n);
+
+    graph = (int**)malloc((n + 2) * sizeof(int*));
+    if (graph == NULL) {
+        free(graph);
+        exit(1);
+    }
+
+    for (int i = 0; i < n + 2; i++) {
+        graph[i] = (int*)malloc((n + 2) * sizeof(int));
+
+        if (*graph == NULL) {
+            for (int j = 0; j < i; j++) {
+                free(graph[j]);
+                exit(1);
+            }
+        }
+        memset(graph[i], 0, ((n + 2) * sizeof(int)));
+    }
+
+    for (int i = 1; i <= n; i++) {
+        char line[26];
+        scanf("%s", line);
+        for (int j = 1; j <= n; j++) {
+            graph[i][j] = line[j - 1] - '0';
+        }
+    }
+
+
+    int size = 0;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (graph[i][j] == 1) {
+                ans = realloc(ans, (++size) * sizeof(int));
+                if (ans == NULL) {
+                    memRelease();
+                }
+                ans[size - 1] = dfs(i, j);
+            }
+        }
+    }
+
+    qsort(ans, size, sizeof(ans[0]), compare);
+
+    printf("%d\n", size);
+    for (int i = 0; i < size; i++) {
+        printf("%d\n", ans[i]);
+    }
+
+    memRelease();
+    free(ans);
+    ans = NULL;
+
+    return 0;
+}
+
+int dfs(int y, int x) {
+
+    int ans = 1;
+
+    graph[y][x] = 0;
+
+    int ny[4] = { 1,-1,0,0 };
+    int nx[4] = { 0,0,1,-1 };
+
+    int nexty;
+    int nextx;
+    for (int i = 0; i < 4; i++) {
+
+        nexty = ny[i] + y;
+        nextx = nx[i] + x;
+
+        if (graph[nexty][nextx] == 1) {
+            ans += dfs(nexty, nextx);
+        }
+    }
+    return ans;
+}
+
+void memRelease() {
+
+    if (ans != NULL) {
+        free(ans);
+        ans = NULL;
+    }
+
+    if (graph == NULL) continue;
+
+    for (int i = 0; i < n + 2; i++) {
+        free(graph[i]);
+    }
+    free(graph);
+    graph = NULL;
+}
+
+int compare(const void* a, const void* b) {
+    int x = *(int*)a;
+    int y = *(int*)b;
+    return (x < y) - (y - x);
+}
 #endif
