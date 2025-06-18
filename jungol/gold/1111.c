c2b53d4725c4cc84f0ecc8b2ea47b535bd4a8ffa
xiingyu <tls3162@naver.com>
2025-06-19 00:20:56 +0900

    asd

--- jungol/gold/1111.c
+++ jungol/gold/1111.c
@@ -7,7 +7,7 @@ int dy[4] = {1,-1,0,0};
 int dx[4] = {0,0,-1,1};
 
 int map[102][102];
-int min[103][103] = {0};
+int min[102][102] = {0};
 int front = 0, rear = 0;
 
 typedef struct _pixel {
@@ -32,7 +32,7 @@ int sqr(int a) {
 
 void bfs() {
 
-    while (front <= rear) {
+    while (front < rear) {
         pixel cur = dequeue();
         if ((cur.y == target_y) && (cur.x == target_x)) {
             if (min[cur.y][cur.x] > cur.cost) {
@@ -41,7 +41,7 @@ void bfs() {
             continue;
         }
 
-        if ((min[cur.y][cur.x] > cur.cost) && (min[cur.y][cur.x] != 0)) {
+        if (min[cur.y][cur.x] > cur.cost) {
             min[cur.y][cur.x] = cur.cost;
         }
         else {
@@ -51,6 +51,9 @@ void bfs() {
         for(int i = 0; i < 4; ++i) {
             int fut_y = cur.y + dy[i];
             int fut_x = cur.x + dx[i];
+            
+            if (fut_y < 1 || fut_y > N || fut_x < 1 || fut_x > N)
+                continue;
 
             int cur_val = map[cur.y][cur.x];
             int fut_val = map[fut_y][fut_x];
@@ -74,9 +77,14 @@ void inputData(void) {
             (void)scanf("%d", &map[i][j]);
         }
     }
+    for (int i = 0; i < 103; i++) {
+        for (int j = 0; j < 103; j++) {
+            min[i][j] = 0x7FFFFFFF;
+        }
+    }
 }
 
-void printAry(int arr[103][103]) {
+void printAry(int arr[102][102]) {
     for (int i = 0; i < 7; ++i) {
         for (int j = 0; j < 7; ++j) {
             printf("%d ", arr[i][j]);
@@ -89,15 +97,19 @@ int main(void) {
     int i,j;
     inputData();
     
-    for (i = 1; i <= N; ++i) {
-        for (j = 1; j <= N; ++j) {
-            if ((i==1)||(i==N)||(j==1)||(j==N)) {
+    for (i = 0; i <= N; ++i) {
+        for (j = 0; j <= N; ++j) {
+            if ((i==0)||(i==N+1)||(j==0)||(j==N+1)) {
                 enqueue(j,i,0);
             }
         }
     }
     bfs();
+    // printf("bfs passed\n");
     printAry(min);
+    // printf("clear\n");
     printf("%d", min[target_y][target_x]);
+    // printf("clear!!\n %d\n%d\n", front, rear);
+
 
 }
