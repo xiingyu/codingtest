3cf52bce9b9d9e829a96a95d1623df8c87b76d14
SKH_gasan <tls3162@naver.com>
2025-06-18 12:31:16 +0900

    upload from gasan

--- jungol/silver/1078.c
+++ jungol/silver/1078.c
@@ -1,3 +1,99 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#define MAX_N (100+2)
+typedef struct _node {
+    int r;
+    int c;
+}node;
+int C, R;  // 열, 행
+int sc, sr;// 열번호, 행번호
+int map[MAX_N][MAX_N];  // 입력, 방문표시(3, 4, 5, ...)
+
+node Queue[MAX_N * MAX_N];
+int front, rear;
+//void enqueue(int r, int c) {
+//    Queue[rear].r = r;
+//    Queue[rear].c = c;
+//    ++rear;
+//}
+
+void enqueue(int r, int c) {
+    Queue[rear++] = (node){ r, c };
+}
+
+node dequeue(void) {
+    return Queue[front++];
+}
+
+// 디버깅 용도
+static void printMap(void) {
+
+    for (int i = 1; i <= R; ++i) {
+        for (int j = 1; j <= C; ++j) {
+            printf("%d ", map[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int dr[4] = { 1, -1, 0, 0 };
+int dc[4] = { 0, 0, 1, -1 };
+
+int BFS(int sr, int sc) {
+    node curr = { 0 };
+    int nr, nc, time=0;
+
+    front = rear = 0;
+    enqueue(sr, sc);
+    map[sr][sc] = 3;
+
+    while (front != rear) {
+        curr = dequeue();
+        time = map[curr.r][curr.c];
+        for (int i = 0; i < 4; ++i) {
+            nr = curr.r + dr[i];
+            nc = curr.c + dc[i];
+            if (map[nr][nc] == 1) {
+                enqueue(nr, nc);
+                map[nr][nc] = time + 1;
+            }
+        }
+    }
+    printMap();
+    return time;
+}
+
+static void inputData(void) {
+    char temp[MAX_N] = { 0 };
+    (void)scanf("%d %d", &C, &R);
+    for (int i = 1; i <= R; i++) {
+        (void)scanf("%s", temp + 1);
+        int* pTmp = map[i];
+        for (int j = 1; j <= C; j++) {
+            pTmp[j] = temp[j] - '0';
+        }
+    }
+    (void)scanf("%d %d", &sc, &sr);
+}
+
+
+
+// 저글링 방사능 오염 - https://jungol.co.kr/problem/1078
+void J1078_solution(void) {
+    (void)freopen("J1078.txt", "r", stdin);
+    inputData();
+    printMap();
+    printf("%d\n", BFS(sr, sc));
+}
+
+
+
+
+#if 0 // 오류수정 요
+
+#include <stdio.h>
 #define MAX_N (100+2)
 
 typedef struct _node {
@@ -10,6 +106,9 @@ int visit[MAX_N][MAX_N];
 
 node Queue[MAX_N * MAX_N];
 int front, rear;
+int C, R;                   // 열 행
+int N, M;                   // 열 행
+int sc, sr;                 // 열 행
 
 //expression1
 // void enqueue(int r, int c, int value) {
@@ -30,11 +129,63 @@ node dequeue(void) {
 }
 
 
+void inputData(void) {
+    char temp[MAX_N] = { 0 };
+    scanf("%d", &N);
 
-int BFS(int sy, int sx) {
+    for (int i = 1; i <= R; i++) {
+        (void)scanf("%s", temp+1);
+        int* pTmp = map[i];
+        for (int j = 1; j <= C; j++) {
+            pTmp[j] = temp[j] - 48;
+        }
+    }
+    (void)scanf("%d %d", &sc,&sr);
+}
+
+int dr[] = {1, -1, 0, 0}; // 상하좌우
+int dc[] = {0, 0, 1. -1};
+
+int BFS(int sr, int sc) {
+    node curr = {0};
+    int nr,nc, time;
+
     front = rear = 0;
-    enqueue(sy,sx);
-    map[sy][sx] = 3;
+    enqueue(sr,sc);
+    map[sr][sc] = 3;
+
+    while(front != rear) {
+        curr = dequeue();
+        for (int i = 0; i < 4; ++i) {
+            nr = curr.r + dr[i];
+            nc = curr.c + dc[i];
+            time = map[curr.r][curr.c];
+            if (map[nr][nc] == 1) {
+                enqueue(nr,nc);
+                map[nr][nc] = time +1;
+            }
+        }
+    }
 
     return 0;
 }
+
+static void printMap(void) {
+    
+    for (int i = 1; i <= R; ++i) {
+        for (int j = 1; j <= C; ++j) {
+            printf("%d ", map[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+
+int main(void) {
+    inputData();
+    BFS(sr,sc);
+    printMap();
+}
+
+#endif
--- jungol/silver/1681.c
+++ jungol/silver/1681.c
@@ -1,21 +1,76 @@
+#if 1
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#define MAX_N (13+1)
+int N;                   // 배달지 개수
+int arr[MAX_N][MAX_N];   // 길:비용
+int visit[MAX_N];        // 방문처리
+int select[MAX_N];       // 디버깅용
+int call_cnt = 0;        // DFS 함수 호출 횟수
+int ans = 0x7FFFFFFF;    // 최대값
 
-int min;
-int visited[15][15] = {0};
-int map[15][15] = {0};
-
-int main(void) {
-    int i,j,n;
-    int temp;
-
-    scanf("%d", &n);
-    
-    for(i = 0; i < n; ++i) {
-        for(j = 0; j < n; ++j) {
-            scanf("%d",&temp);
+static void inputData(void) {
+    (void)scanf("%d", &N);
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            (void)scanf("%d", &arr[i][j]);
         }
-
     }
 }
+
+// 디버깅 용도
+static void printArr(void) {
+    for (int i = 1; i <= N; ++i) {
+        for (int j = 1; j <= N; ++j) {
+            printf("%2d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+// 어떤 경로로 방문했는지 기록
+static void printSelect(void) {
+    for (int i = 1; i < N; ++i) {
+        printf("%d ", select[i]);
+    }
+    printf("  ans : %d\n", ans);
+}
+
+// cnt : 방문한 개수, curr : 현재위치, cost : 현재까지의 비용
+void DFS(int cnt, int curr, int cost) {
+    
+    if (cost >= ans) return;
+    ++call_cnt;
+    if (cnt == N) {
+        int temp = arr[curr][1];
+        if (temp == 0) return;
+        if (cost + temp < ans) {
+            ans = cost + temp; 
+            printSelect();
+        }
+        return;
+    }
+
+    for (int i = 2; i <= N; i++) {
+        if (visit[i] == 1 || arr[curr][i] == 0) continue;
+        select[cnt] = i;
+        visit[i] = 1;
+        DFS(cnt + 1, i, cost + arr[curr][i]);
+        visit[i] = 0;
+    }
+}
+
+// 해밀턴 순환회로 - https://jungol.co.kr/problem/1681
+void J1681_solution(void) {
+    (void)freopen("J1681.txt", "r", stdin);
+    inputData();
+    //printArr();
+    visit[1] = 1;
+    DFS(1, 1, 0);
+    printf("%d\n", ans);
+    printf("call_cnt : %d\n", call_cnt);
+}
+
+
+#endif
