86640c1b638a80b0168746cf8908af37b7fd4569
SKH_gasan <tls3162@naver.com>
2025-06-24 18:56:17 +0900

    upload from gasan

---
+++ baekjoon/silver/silver 2/1260.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n,m,v;
+int dfs_history[1000] = {0,};
+int bfs_history[1000] = {0,};
+
+
+void dfs() {
+
+}
+
+void bfs() {
+
+}
+
+
+int main(void) {
+
+    cin >> n >> m >> v;
+    vector<vector<int>> graph(2, vector<int>(m, 0));
+
+
+}
