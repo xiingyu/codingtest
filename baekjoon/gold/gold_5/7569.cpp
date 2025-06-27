00a17a5ce4b5262bf683a919500382010f3633cf
SKH_gasan <tls3162@naver.com>
2025-06-27 18:24:18 +0900

    upload

--- baekjoon/gold/gold_5/7569.cpp
+++ baekjoon/gold/gold_5/7569.cpp
@@ -8,12 +8,12 @@ int m,n,h;
 void get_table() {
     int i,j,k;
     int temp;
-    table.resize(h+2);
-    for(i =0; i < m; ++i) {
-        table[i].resize(m+2);
-        for(j =0; j < h; ++j) {
-            table[i][j].resize(k+2);
-            for(k =0; k < n; ++k) {
+    table.resize(h);
+    for(i =0; i < h; ++i) {
+        table[i].resize(n);
+        for(j =0; j < n; ++j) {
+            table[i][j].resize(m);
+            for(k =0; k < m; ++k) {
                 cin >> temp;
                 table[i][j][k] = temp;
             }
@@ -23,9 +23,9 @@ void get_table() {
 
 void print_table() {
     int i,j,k;
-    for(i =0; i < m; ++i) {
-        for(j =0; j < h; ++j) {
-            for(k =0; k < n; ++k) {
+    for(i =0; i < h; ++i) {
+        for(j =0; j < n; ++j) {
+            for(k =0; k < m; ++k) {
                 cout << table[i][j][k] << " ";
             }
             cout << endl;
@@ -39,6 +39,7 @@ int main(void) {
 
 
     cin >> m >> n >> h;
+    // cout << m << n << h << endl;
     get_table();
     print_table();
 
