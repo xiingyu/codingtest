94474fa30f2d20b9e872ecf7c9eaa848c68d1905
xiingyu <tls3162@naver.com>
2025-07-27 14:51:52 +0900

    upload from gram

---
+++ baekjoon/gold/gold_5/1916.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int N, M, start, goal;
+vector<bool> visited;
+vector<vector<int>> nexts,costs;
+vector<int> takeCosts;
+queue<int> que;
+
+void init() {
+    int i, s,e,c;
+    cin >> N >> M;
+
+
+    for(i = 0; i < M; ++i) {
+        cin >> s >> e >> c;
+        
+    }
+
+}
+
+int main() {
+    
+}
