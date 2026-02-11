d0a8b77f0de7854a6e3b8f5052602c877afb6918
desktop <tls3162@naver.com>
2026-02-11 22:40:12 +0900

    upload from home

--- baekjoon/gold/gold_4/14938.cpp
+++ baekjoon/gold/gold_4/14938.cpp
@@ -3,74 +3,62 @@
 #define INF 1e9
 using namespace std;
 
-int N,M,R;
-int maxScore = INF;
+int N, M, R;
 vector<int> scores;
 vector<vector<int>> table;
+// vector<vector<int>> nxt;
 
 void init() {
     int a,b,c;
-    cin >> N >> M >> R;
-    scores.resize(N+1);
-    for(int i = 1; i <= N; ++i) {
+    cin >> N >> M >>R;
+
+    scores.resize(N+1, 0);
+    for(int i = 1; i <=N; ++i) {
         cin >> scores[i];
     }
 
     table.resize(N+1, vector<int>(N+1, INF));
-    for(int i = 1; i <= N; ++i) {
-        for(int j = 1; j <= N; ++j) {
-            cin >> a >> b >> c;
-            table[a][b] = min(table[a][b], c);
-            table[b][a] = min(table[b][a], c);
+    // nxt.resize(N+1, vector<int>(N+1, 0));
+
+    for(int i = 1; i <= R; ++i) {
+        cin >> a >> b >> c;
+        table[a][b] = min(table[a][b], c);
+        table[b][a] = min(table[b][a], c);
+        // nxt[a][b] = b; nxt[b][a] = a;
+    }
+}
+
+void logic() {
+    for(int k = 1; k <= N; ++k) {
+        for(int i = 1; i <=N; ++i) {
+            for(int j = 1; j <=N; ++j) {
+                if(table[i][k] + table[k][j] < table[i][j]) {
+                    table[i][j] = table[i][k] + table[k][j];
+                    // nxt[i][j] = nxt[i][k];
+                }
+            }
         }
     }
 }
 
-int dfs(int s, int cost, int point, vector<int>& visited) {
-    int totalScore = scores[s];
 
-    cout << "s : " <<  s  << " cost : " << cost  << " point : "  << point << endl;
-    
-    for(int i = 1; i <= N; ++i) {
-        if(visited[i])  continue;
-        if(table[s][i] != INF) {
-            if(cost + table[s][i] > M) continue;
-            
-
-            visited[i] = true;
-            // totalScore = max(dfs(i, cost + table[s][i], point + scores[i], visited), totalScore);
-            // totalScore += scores[i];
-            // dfs(i, cost + table[s][i], totalScore, visited);
-            totalScore += dfs(i, cost + table[s][i], point + scores[i], visited);
-            cout << "s : " << s << " total : " << totalScore <<endl;
-            visited[i] = false;
-            // totalScore -= scores[i];
-
-        }
-
-    }
-    // cout << totalScore << endl;
-    return totalScore;
-}
 
 int main() {
-    int maxScore = 0;
-    vector<int> visited;
-
     init();
-    visited.resize(N+1, false);
-    // for(int i = 1; i <= N; ++i) {
-    //     // cout << "number " << i << " start!" << endl;
-    //     visited[i] = true;
-    //     maxScore = max(maxScore, dfs(i, 0, scores[i], visited));
-    //     visited[i] = false;
-    // }
-        int i = 1;
-        visited[i] = true;
-        maxScore = max(maxScore, dfs(i, 0, scores[i], visited));
-        visited[i] = false;
+    logic();
 
-    cout << maxScore;
+    int cnt, answer = 0;
 
-    return 0;
+    for(int i = 1; i < N+1; ++i) {
+        cnt = scores[i];
+        for(int j = 1; j < N+1; ++j) {
+            if(i != j && table[i][j] <= M) {
+                cnt += scores[j];
+            }
+        }
+        answer = max(answer, cnt);
+    }
+
+    cout << answer;
+
 }
