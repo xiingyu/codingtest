31f83a3b5a0e48a1a55255b1df90a6e5c862a699
xiingyu <tls3162@naver.com>
2026-02-20 20:31:42 +0900

    upload from caffee

--- baekjoon/platinum/platinum_5/14003.cpp
+++ baekjoon/platinum/platinum_5/14003.cpp
@@ -5,81 +5,57 @@ using namespace std;
 
 int N;
 vector<int> table;
-vector<int> dp;
-vector<int> befTable;
+vector<int> ansArr;
+vector<int> indexTable;
+
 
 void init() {
-    int temp;
     cin >> N;
+
+    int temp;
     for(int i = 0; i < N; ++i) {
-        cin  >> temp;
+        cin >> temp;
         table.push_back(temp);
     }
 
-    dp.resize(N, 1);
-    befTable.resize(N, -1);
-}
-
-int logic() {
-    int mxIndex = 0;
-    int mxDPs = 0;
-    for(int i = 1; i < N; ++i) {
-        for(int j = i - 1; j >= 0; --j) {
-            if(table[i] > table[j] && dp[i] < dp[j] + 1) {
-                dp[i] = dp[j] + 1;
-                befTable[i] = j;
-                if(mxDPs < dp[i]) {
-                    mxDPs = dp[i];
-                    mxIndex = i;
-                }
-            }
-        }
-    }
-    return mxIndex;
-}
-
-void printTable() {
-    cout << "befTable :" << endl;
-    for(int i = 0; i < N; ++i) {
-        cout << befTable[i] << " ";
-    }
-    cout << endl;
-    cout << "dp :" << endl;
-    for(int i = 0; i < N; ++i) {
-        cout << dp[i] << " ";
-    }
-    cout << endl;
+    indexTable.assign(N, 0);
 }
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     init();
-    int mxIdx = logic();
-    // printTable();
-    // int mx = *max_element(dp.begin(), dp.end());
-    // for(int i = N-1; i >= 0; --i) {
-    //     if(dp[i] == mx) {
-    //         mxIdx = i;
-    //         break;
-    //     }
-    // }
 
-    int idx = mxIdx;
-    vector<int> ans;
-    while(true) {
-        ans.push_back(table[idx]);
-
-        if(befTable[idx] == -1) break;
-        idx = befTable[idx];
-        
+    int temp;   int idx = 1;    int idxTemp = 0;
+    ansArr.push_back(table[0]);
+    for(int i = 1; i < N; ++i) {
+        temp = table[i];
+        if(temp > ansArr[idx - 1]) {
+            ansArr.push_back(temp);
+            indexTable[i] = idx;
+            idx++;
+        } else {
+            idxTemp = lower_bound(ansArr.begin(), ansArr.begin() + idx, temp) - ansArr.begin();
+            ansArr[idxTemp] = temp;
+            indexTable[i] = idxTemp;
+        }
     }
 
-    cout << ans.size() << '\n';
-    for(int i = (int)ans.size() - 1; i >= 0; --i) {
-        cout << ans[i] << " ";
+    vector<int> decodeTable;
+
+    cout << idx << '\n';
+    for(int i = N - 1; i >= 0; --i) {
+        if(indexTable[i] == idx - 1) {
+            decodeTable.push_back(table[i]);
+            --idx;
+        }
     }
-    
+
+    for(int i = (int)decodeTable.size() - 1; i >= 0; --i) {
+        cout << decodeTable[i] << ' ';
+    }
+
+
+
 
     return 0;
 }
